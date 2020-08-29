74eae7744af3a7dcf4e80afaedd5e8e260d1ef93
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-08-29 17:06:32 +0200

    merge marker removed

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -11,17 +11,12 @@ export class SocketService {
 
   connect(token: string, username: string) {
     const config: SocketIoConfig = {
-<<<<<<< HEAD
       url: '/notify',
       options: {
         extraHeaders: {
           Authorization: token
         }
       }
-=======
-      url: 'http://localhost:80/notify'
-      // url: window.location.host + '/notify'
->>>>>>> 71a102e277ff722c8049787a55e104ed33521fdd
     };
     this.socket = new Socket(config);
 
